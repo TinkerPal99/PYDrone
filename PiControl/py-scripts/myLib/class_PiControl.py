2485c802c414ced2cc0e9c20fe6443db5717593c
Eising <marwin.eising@outlook.de>
2019-11-23 12:14:30 +0100

    added logging
    for overview and improvement I added logging to the oop-new__init__ of PiControl

--- PiControl/py-scripts/myLib/class_PiControl.py
+++ PiControl/py-scripts/myLib/class_PiControl.py
@@ -1,13 +1,13 @@
 #! usr/bin/env/python
 import lib_gy521
 import RPi.GPIO as GPIO
-import urllib, time, datetime
+import urllib2, time, datetime
 
 class PiControl:
     def __init__(self, address, path):
-        self.gyro_out = []
+        self.__gyro_out = []
         self.destination_address = address
-        self.cam = PiCamera()
+        self.__cam = PiCamera()
         self.path_for_savings = path
 #----------------------------cam----------------------------------------------------------------
     def setSettings_of_Cam(width, height, framerate):
@@ -74,51 +74,51 @@ class PiControl:
 #------------------------------gyro---------------------------------------------
     def gyro_read(self):
 
-        gyroskop_xout = lib_gy521.read_word_2c(0x43)
-        gyroskop_yout = lib_gy521.read_word_2c(0x45)
-        gyroskop_zout = lib_gy521.read_word_2c(0x47)
+        __gyroskop_xout = lib_gy521.read_word_2c(0x43)
+        __gyroskop_yout = lib_gy521.read_word_2c(0x45)
+        __gyroskop_zout = lib_gy521.read_word_2c(0x47)
 
         # print("gyroskop_xout: ", ("%5d" % gyroskop_xout), " skaliert: ", (gyroskop_xout / 100))
         # print("gyroskop_yout: ", ("%5d" % gyroskop_yout), " skaliert: ", (gyroskop_yout / 100))
         # print("gyroskop_zout: ", ("%5d" % gyroskop_zout), " skaliert: ", (gyroskop_zout / 100))
 
-        gy_x_skal = gyroskop_xout / 100
-        gy_y_skal = gyroskop_yout / 100
-        gy_z_skal = gyroskop_zout / 100
+        __gy_x_skal = gyroskop_xout / 100
+        __gy_y_skal = gyroskop_yout / 100
+        __gy_z_skal = gyroskop_zout / 100
         time.sleep(1)
 
-        self.gyro_out.append(gy_x_skal)
-        self.gyro_out.append(gy_y_skal)
-        self.gyro_out.append(gy_z_skal)
+        self.__gyro_out.append(gy_x_skal)
+        self.__gyro_out.append(gy_y_skal)
+        self.__gyro_out.append(gy_z_skal)
 
         # print (gy_x_skal)
         # print (gy_y_skal)
         # print (gy_z_skal)
 
-        return self.gyro_out
+        return self.__gyro_out
 
     def gyro_CallOnRead(self, gyro_out):
-        if self.gyro_out[1] >= 40:
+        if self.__gyro_out[1] >= 40:
             print("forward")
             self.call_address("RPS1.php")
-            # urllib.urlopen("http://192.168.8.200/main/RPS1.php")
-        elif self.gyro_out[1] <= -40:
+            urllib.urlopen("http://192.168.8.200/main/RPS1.php")
+        elif self.__gyro_out[1] <= -40:
             print("backward")
             self.call_address("RPS5.php")
-            # urllib.urlopen("http://192.168.8.200/main/RPS5.php")
-        elif self.gyro_out[0] >= 50:
+            urllib.urlopen("http://192.168.8.200/main/RPS5.php")
+        elif self.__gyro_out[0] >= 50:
             print("right")
             self.call_address("RPS5.php")
-            # urllib.urlopen("http://192.168.8.200/main/RPS4.php")
-        elif self.gyro_out[0] <= -50:
+            urllib.urlopen("http://192.168.8.200/main/RPS4.php")
+        elif self.__gyro_out[0] <= -50:
             print("left")
             self.call_address("RPS3.php")
-            # urllib.urlopen("http://192.168.8.200/main/RPS3.php")
-        elif self.gyro_out[2] >= 50:
+            urllib.urlopen("http://192.168.8.200/main/RPS3.php")
+        elif self.__gyro_out[2] >= 50:
             print("down")
             self.call_address("RPS2.php")
-            # urllib.urlopen("http://192.168.8.200/main/RPS2.php")
-        elif self.gyro_out[2] <= -50:
+            urllib.urlopen("http://192.168.8.200/main/RPS2.php")
+        elif self.__gyro_out[2] <= -50:
             print("up")
             self.call_address("RPS2.php")
-            # urllib.urlopen("http://192.168.8.200/main/RPS2.php")
+            urllib.urlopen("http://192.168.8.200/main/RPS2.php")
--- PiControl/py-scripts/new__init__.py
+++ PiControl/py-scripts/new__init__.py
@@ -1,30 +1,63 @@
 # coding=utf-8
 # !usr/bin/env/python
+import datetime
 
 import RPi.GPIO as GPIO
 from myLib import lib_gy521, class_PiControl
-import time
+import time, urllib2, logging, sys, os
 
 GPIO.setmode(GPIO.BOARD)
+#----------------------------logging_basicconfig---------------------
+logging.basicConfig(
+    filename = "logs/init.log",
+    filemode ="a")
+#Ausgabe auf terminal
+__log_handler = logging.StreamHandler(sys.stdout)
 
+#setzen eines eigenen Loggerfromats
+__log_form = logging.Formatter("%(asctime)s %(levename)s: %(messages)s", "%d.%m.%Y %H:%M:%S")
+__log_handler.setFormatter(__log_form)
+__logger = logging.getLogger()
+__logger.addHandler(__log_handler)
+#Standardloglevel einsetzen
+__logger.setLevel(logging.DEBUG)
+
+currentDir = os.getcwd()
+#TODO Remove unused datetime
+#dateTimeObj = datetime.now()
+#datestamp = dateTimeObj.strftime("%Y-%m-%d")
 # --------------------------Preparation---------------------------------------------------------------------------------
-piControl = class_PiControl.PiControl("http://192.168.8.200/main/", "/savings")
-
+callIP = "http://192.168.8.200/main/"
+saveSpace = "/savings"
 LAMP_Y = 1
 LAMP_G = 1
+
+piControl = class_PiControl.PiControl(callIP, saveSpace)
+logging.log(logging.INFO,"CallIP set as " + callIP +". Storage for savings set as " + currentDir + saveSpace)
+
 
 Taster = 20
 GPIO.setup(LAMP_Y, GPIO.IN)
 GPIO.setup(LAMP_G, GPIO.IN)
+
 # -----------------------------MAIN-------------------------------------------------------------------------------------
 while True:
-    if GPIO.input(Taster) == GPIO.HIGH:
-        while True:
-            piControl.gyro_CallOnRead(piControl.gyro_read())
-            time.sleep(0.5)
-            if GPIO.input(Taster) == GPIO.HIGH:
-                GPIO.output(LAMP_Y) = GPIO.HIGH
-                GPIO.output(LAMP_G) = GPIO.LOW
-                time.sleep(5)
-                break
-    time.sleep(1)
+    try:
+        if GPIO.input(Taster) == GPIO.HIGH:
+            while True:
+                piControl.gyro_CallOnRead(piControl.gyro_read())
+                time.sleep(0.5)
+                if GPIO.input(Taster) == GPIO.HIGH:
+                    GPIO.output(LAMP_Y) = GPIO.HIGH
+                    GPIO.output(LAMP_G) = GPIO.LOW
+                    time.sleep(5)
+                    break
+        time.sleep(0.01)
+    except urllib2.URLError:
+        logging.log(logging.DEBUG,"Fehler - urllib")
+    except KeyboardInterrupt:
+        logging.log(logging.INFO,"Abbruch - keybord interrupt")
+        logging.shutdown()
+    except Exception as unknown:
+        logging.log(logging.CRITICAL,"Abbruch - Kritischer unbekannter Fehler")
+        logging.shutdown()
