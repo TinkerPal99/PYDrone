4589db417a41c91f6647adecfcade79b23967a8a
Eising <marwin.eising@outlook.de>
2019-11-25 07:55:44 +0100

    Update new__init__.py
    Added second log-level for info, redacted signla lamps and button

--- PiControl/py-scripts/new__init__.py
+++ PiControl/py-scripts/new__init__.py
@@ -9,6 +9,21 @@ import time, urllib2, logging, sys, os
 GPIO.setmode(GPIO.BOARD)
 #----------------------------logging_basicconfig---------------------
 logging.basicConfig(
+    filename = "logs/init_error.log",
+    filemode ="a")
+#Ausgabe auf terminal
+__log_handler = logging.StreamHandler(sys.stdout)
+
+#setzen eines eigenen Loggerfromats
+__log_form = logging.Formatter("%(asctime)s %(levename)s: %(messages)s", "%d.%m.%Y %H:%M:%S")
+__log_handler.setFormatter(__log_form)
+__logger_error = logging.getLogger()
+__logger_error.addHandler(__log_handler)
+#Standardloglevel einsetzen
+__logger_error.setLevel(logging.DEBUG)
+
+
+logging.basicConfig(
     filename = "logs/init.log",
     filemode ="a")
 #Ausgabe auf terminal
@@ -20,7 +35,7 @@ __log_handler.setFormatter(__log_form)
 __logger = logging.getLogger()
 __logger.addHandler(__log_handler)
 #Standardloglevel einsetzen
-__logger.setLevel(logging.DEBUG)
+__logger.setLevel(logging.INFO)
 
 currentDir = os.getcwd()
 #TODO Remove unused datetime
@@ -29,29 +44,18 @@ currentDir = os.getcwd()
 # --------------------------Preparation---------------------------------------------------------------------------------
 callIP = "http://192.168.8.200/main/"
 saveSpace = "/savings"
-LAMP_Y = 1
-LAMP_G = 1
 
 piControl = class_PiControl.PiControl(callIP, saveSpace)
-logging.log(logging.INFO,"CallIP set as " + callIP +". Storage for savings set as " + currentDir + saveSpace)
+logging.log(logging.INFO,"CallIP set as " + piControl.get_call_address() +". Storage for savings set as " + currentDir + saveSpace)
 
-
-Taster = 20
-GPIO.setup(LAMP_Y, GPIO.IN)
-GPIO.setup(LAMP_G, GPIO.IN)
 
 # -----------------------------MAIN-------------------------------------------------------------------------------------
 while True:
     try:
-        if GPIO.input(Taster) == GPIO.HIGH:
-            while True:
-                piControl.gyro_CallOnRead(piControl.gyro_read())
-                time.sleep(0.5)
-                if GPIO.input(Taster) == GPIO.HIGH:
-                    GPIO.output(LAMP_Y) = GPIO.HIGH
-                    GPIO.output(LAMP_G) = GPIO.LOW
-                    time.sleep(5)
-                    break
+        while True:
+            piControl.gyro_CallOnRead(piControl.gyro_read())
+            time.sleep(0.05)
+            logging.log(logging.INFO, " Gyro: " + piControl.gyro_read())
         time.sleep(0.01)
     except urllib2.URLError:
         logging.log(logging.DEBUG,"Fehler - urllib")
